1ff84707eb579d08e07edb7db1fb1fbdf105fc16
daneedev <dan@danee.dev>
2025-08-03 18:33:19 +0200

    index.d.ts renamed to index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,6 +14,7 @@ export interface GiveawayOptions {
     prize: string;
     winnerCount: number;
     duration: number;
+    requirements?: GiveawayRequirements;
 }
 
 export interface GiveawayManagerOptions {
@@ -26,4 +27,11 @@ export type GiveawayEvents = {
     giveawayEnded: (giveaway: GiveawayData, winners: string[]) => void;
     giveawayRerolled: (giveaway: GiveawayData, winners: string[]) => void;
     giveawayEdited: (giveaway: GiveawayData, updated: GiveawayData) => void;
+}
+
+export interface GiveawayRequirements {
+    requiredRoles?: string[];
+    accountAgeMin?: number;
+    joinedServerBefore?: number;
+    custom?: (userId: string) => Promise<boolean>;
 }
